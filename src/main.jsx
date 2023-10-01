97293dc2c4b9795be73c13850f96e83bb884be21
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-02 01:31:35 +0600

    created login and register page and linked to router

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,31 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+
+    ]
   },
 ]);
 
